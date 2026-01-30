bb202e8f21851ddf06de09915cafd444bedeb9f3
Julien Dutschke <jdutschk@student.42mulhouse.fr>
2026-01-30 22:58:55 +0100

    Refactor countdown function and add decrement logic

--- 0/01/ft_countdown.ml
+++ 0/01/ft_countdown.ml
@@ -1,12 +1,14 @@
-let ft_countdown (start_value : int) =
-  if start_value < 0 then
-  (
-    print_int 0;
-    print_char '\n'
-  )
-  else
-    for i = start_value downto 0
-		do
-			print_int i;
-			print_char '\n'
-		done
+let print_null () = print_int 0
+
+let print_number (x : int) = print_int x ; print_char '\n'
+
+let rec ft_decrem (value : int) =
+  match value with
+  | 0 ->
+      print_null ()
+  | otherValue ->
+      print_number otherValue;
+      ft_decrem (otherValue - 1)
+
+let () =
+  ft_decrem 10
